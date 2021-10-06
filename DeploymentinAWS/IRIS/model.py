4a53534b89269dcfca4c9baf21a35aa0f3d3754b
Chaitanya Majety <chaitanyamajety1998@gmail.com>
2021-10-06 22:56:58 +0530

    IRIS AWS deployment

--- DeploymentinAWS/IRIS/model.py
+++ DeploymentinAWS/IRIS/model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
-from sklearn.externals import joblib
+import joblib
 data = pd.read_csv('iris.csv')
 print(data.head())
 X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
